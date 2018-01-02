afdf834d272eebc322987338f3cb0a42975d681b
JaonFanwt <fanwt883188@gmail.com>
2018-01-02 23:39:26 +0800

    1.添加自定义popNavigationController的层级。

---
+++ WtCore/Classes/Category/UIViewController+WtExtension.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+WtExtension.h
+//  WtCore
+//
+//  Created by wtfan on 2018/1/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (WtExtension)
+- (UIViewController *)wtPreviousViewController;
+- (void)wtPushViewController:(UIViewController *)toViewCtrl animated:(BOOL)animated getRidNum:(NSUInteger)getRidNum;
+@end
