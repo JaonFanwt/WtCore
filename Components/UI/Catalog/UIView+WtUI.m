14bb3f72d2caedf1ca790d63fdb953ea0188a1c0
JaonFanwt <fanwt883188@gmail.com>
2018-01-07 22:18:57 +0800

    1.whenTapped事件由多个手势改为唯一手势。

--- Components/UI/Catalog/UIView+WtUI.m
+++ Components/UI/Catalog/UIView+WtUI.m
@@ -13,13 +13,19 @@
 #import "UIGestureRecognizer+WtUI.h"
 
 @implementation UIView (WtUI)
-
 #pragma mark public
 - (void)wtWhenTapped:(void (^)(void))block {
     self.userInteractionEnabled = YES;
-    UITapGestureRecognizer *tapGesture = [UITapGestureRecognizer wtWithHandler:^(UIGestureRecognizer *sender, UIGestureRecognizerState state, CGPoint location) {
+
+    UITapGestureRecognizer *tapGesture = objc_getAssociatedObject(self, _cmd);
+    if (tapGesture) {
+        [self removeGestureRecognizer:tapGesture];
+    }
+
+    tapGesture = [UITapGestureRecognizer wtWithHandler:^(UIGestureRecognizer *sender, UIGestureRecognizerState state, CGPoint location) {
         wtDispatch_in_main(block);
     }];
+    objc_setAssociatedObject(self, _cmd, tapGesture, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     [self addGestureRecognizer:tapGesture];
 }
 @end
