a61946680247c5e06b435940911fec993ac60b91
JaonFanwt <fanwt883188@gmail.com>
2018-01-07 23:40:34 +0800

    1.WtUI添加获取及修改x、y、width、height。

--- Components/UI/Catalog/UIView+WtUI.h
+++ Components/UI/Catalog/UIView+WtUI.h
@@ -10,5 +10,15 @@
 #import "WtDelegateProxy.h"
 
 @interface UIView (WtUI)
+@property (nonatomic, assign) CGFloat wtX;
+@property (nonatomic, assign, readonly) CGFloat wtMinX;
+@property (nonatomic, assign, readonly) CGFloat wtMidX;
+@property (nonatomic, assign, readonly) CGFloat wtMaxX;
+@property (nonatomic, assign) CGFloat wtY;
+@property (nonatomic, assign, readonly) CGFloat wtMinY;
+@property (nonatomic, assign, readonly) CGFloat wtMidY;
+@property (nonatomic, assign, readonly) CGFloat wtMaxY;
+@property (nonatomic, assign) CGFloat wtWidth;
+@property (nonatomic, assign) CGFloat wtHeight;
 - (void)wtWhenTapped:(void (^)(void))block;
 @end
--- Components/UI/Catalog/UIView+WtUI.m
+++ Components/UI/Catalog/UIView+WtUI.m
@@ -14,6 +14,70 @@
 
 @implementation UIView (WtUI)
 #pragma mark public
+- (CGFloat)wtX {
+    return self.frame.origin.x;
+}
+
+- (CGFloat)wtMinX {
+    return CGRectGetMinX(self.frame);
+}
+
+- (CGFloat)wtMidX {
+    return CGRectGetMidX(self.frame);
+}
+
+- (CGFloat)wtMaxX {
+    return CGRectGetMaxX(self.frame);
+}
+
+- (void)setWtX:(CGFloat)wtX {
+    CGRect rect = self.frame;
+    rect.origin.x = wtX;
+    self.frame = rect;
+}
+
+- (CGFloat)wtY {
+    return self.frame.origin.y;
+}
+
+- (CGFloat)wtMinY {
+    return CGRectGetMinY(self.frame);
+}
+
+- (CGFloat)wtMidY {
+    return CGRectGetMidY(self.frame);
+}
+
+- (CGFloat)wtMaxY {
+    return CGRectGetMaxY(self.frame);
+}
+
+- (void)setWtY:(CGFloat)wtY {
+    CGRect rect = self.frame;
+    rect.origin.y = wtY;
+    self.frame = rect;
+}
+
+- (CGFloat)wtWidth {
+    return self.frame.size.width;
+}
+
+- (void)setWtWidth:(CGFloat)wtWidth {
+    CGRect rect = self.frame;
+    rect.size.width = wtWidth;
+    self.frame = rect;
+}
+
+- (CGFloat)wtHeight {
+    return self.frame.size.height;
+}
+
+- (void)setWtHeight:(CGFloat)wtHeight {
+    CGRect rect = self.frame;
+    rect.size.height = wtHeight;
+    self.frame = rect;
+}
+
 - (void)wtWhenTapped:(void (^)(void))block {
     self.userInteractionEnabled = YES;
 
